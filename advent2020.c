f1cf37ea32a5e1afa9d86ca8fc6eac207fd44700
YYY <XXX>
2020-12-27 14:13:16 -0600

    15s per 10k iteration faster version

--- advent2020.c
+++ advent2020.c
@@ -1,109 +1,100 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
-#define N 100
-#define M 200//10*1000*1000
+#define N (1000*1000)
+#define M (10000)//(10*1000*1000)
 
-#define CO(x, y) ((y) * 2 + (x))
+struct LinkedList {
+    struct LinkedList* prv;
+    struct LinkedList* nxt;
+    int val;
+};
+typedef struct LinkedList Node;
 
-int find_first(int s, int* l, int i)
+Node* find_first(Node* ll, int s)
 {
-    for (int j = 0; j < N; ++j) {
-	if (l[CO(i,j)] == s) {
-	    return j;
+    Node* d = ll;
+    for (int i = 0; i < N; ++i) {
+	if (s == d->val) {
+	    return d;
 	}
+	d = d->nxt;
     }
 
-    return -1;
+    assert(0);
+    return NULL;
+}
+
+void dump_list(Node* ll)
+{
+    Node* cur = ll;
+    for (int i = 0; i < N; ++i) {
+	printf("%d ", cur->val);
+	cur = cur->nxt;
+    }
+    printf("\n");
 }
 
 void p23II(const char* input)
 {
     int input_length = strlen(input);
 
-    int* l = malloc(sizeof(int) * N * 2);
-
+    Node* ll = malloc(sizeof(Node) * N);
+    
     for (int i = 0; i < N; ++i) {
 	if (i < input_length) {
-	    l[CO(0, i)] = input[i] - '0';
+	    ll[i].val = input[i] - '0';
 	}
 	else {
-	    l[CO(0, i)] = i + 1;
+	    ll[i].val = i + 1;
 	}
+
+	ll[i].prv = &ll[(i-1) % N];
+	ll[i].nxt = &ll[(i+1) % N];
     }
 
-    int m = 1;
-    int x = N;
-    int n = x;
-    int i = 0, j = 0;
-    int pu[3] = {0, 0, 0};
-
+    Node* cur = &ll[0];
     for (int move = 0; move < M; ++move)    
     {
-	/* if (move <= 4) { */
-	/*     for (int z = 0; z < N; ++z) { */
-	/* 	printf("%d ", a[i][z]); */
-	/*     } */
-	/*     printf("\n"); */
-	/* } */
+	/* printf("start: "); */
+	/* dump_list(ll); */
 	
-	/* if (move % 1000 == 0) { */
-	/*     printf("on move %d\n", move); */
-	/* } */
+	Node* nxt1 = cur->nxt;
+	Node* nxt2 = nxt1->nxt;
+	Node* nxt3 = nxt2->nxt;
+	Node* nxt4 = nxt3->nxt;
+
+	int s = cur->val;
+	do {
+	    --s;
+	    if (s < 1) s = N;
+	} while (s == nxt1->val || s == nxt2->val || s == nxt3->val);
 	
-	int s = l[CO(i,j)] - 1;
-	if (s < m) s = x;
-	int oi = (i+1) % 2;
-	pu[0] = (j+1) % n, pu[1] = (j+2) % n, pu[2] = (j+3) % n;
+	Node* dst = find_first(ll, s);
+	assert(dst && dst != nxt1 && dst != nxt2 && dst != nxt3);
 
-	int k = -1;
-	while (1) {
-	    k = find_first(s, l, i);
+	cur->nxt = nxt4;
+	nxt4->prv = cur;
 
-	    if (k < 0 || k == pu[0] || k == pu[1] || k == pu[2]) {
-		s = (s-1) < m ? x : (s-1);
-		continue;
-	    }
-	    break;
-	}
+	Node* ndst = dst->nxt;
+	dst->nxt = nxt1;
+	nxt1->prv = dst;
+	nxt3->nxt = ndst;
+	ndst->prv = nxt3;
 
-	int jj = j;
-	l[CO(oi, jj)] = l[CO(i, jj)];
-	int layed = 1;
-
-	int jjo = (jj + 1) % n;
-	int jji = (jj + 4) % n;
-	while (1) {
-	    l[CO(oi, jjo)] = l[CO(i, jji)];
-	    ++layed;
-	    if (jji == k) {
-		jji = (jji + 1) % n, jjo = (jjo + 1) % n;
-		break;
-	    }
-	    jji = (jji + 1) %n, jjo = (jjo + 1) % n;
-	}
-
-	for (int u = 0; u < 3; ++u) {
-	    l[CO(oi, jjo)] = l[CO(i, pu[u])];
-	    ++layed;
-	    jjo = (jjo + 1) % n;
-	}
-
-	while (layed < n) {
-	    l[CO(oi, jjo)] = l[CO(i, jji)];
-            ++layed;
-            jji = (jji + 1) % n, jjo = (jjo + 1) % n;
-	}
-
-        i = (i+1) % 2;
-        j = (j+1) % n;
+	cur = nxt4;
+	/* printf("stop: "); */
+	/* dump_list(ll); */
     }
 
-    int j1 = find_first(1, l, i);
-    int fl1 = l[CO(i, (j1+1) % n)];
-    int fl2 = l[CO(i, (j1+2) % n)];
-    long fl3 = fl1 * fl2;
+    Node* j1 = find_first(ll, 1);
+    Node* j1nxt1 = j1->nxt;
+    Node* j1nxt2 = j1nxt1->nxt;
+    int fl1 = j1nxt1->val;
+    int fl2 = j1nxt2->val;
+    int fl3 = fl1 * fl2;
     printf("%d * %d = %ld\n", fl1, fl2, fl3);
 }
 
